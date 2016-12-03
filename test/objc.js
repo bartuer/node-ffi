40f26ed91b1a16f71344c84a9ac73e619e2b4af2
bartuer zhou <bazhou@microsoft.com>
2016-12-04 00:34:17 +0800

    format

--- test/objc.js
+++ test/objc.js
@@ -1,4 +1,3 @@
-
 var assert = require('assert');
 var ref = require('ref');
 var ffi = require('../');
@@ -13,43 +12,41 @@ var Class = voidPtr;
 
 if (ffi.HAS_OBJC) {
 
-  describe('@try / @catch', function () {
-    // not entirely sure why this works, but we have to load `Foundation` first,
-    // otherwise Objective-C exceptions will not work. Magic!
-    // https://github.com/node-ffi/node-ffi/issues/195
-    var lib = ffi.DynamicLibrary('/System/Library/Frameworks/Foundation.framework/Versions/Current/Foundation')
+    describe('@try / @catch', function () {
+        // not entirely sure why this works, but we have to load `Foundation` first,
+        // otherwise Objective-C exceptions will not work. Magic!
+        // https://github.com/node-ffi/node-ffi/issues/195
+        var lib = ffi.DynamicLibrary('/System/Library/Frameworks/Foundation.framework/Versions/Current/Foundation');
 
-    afterEach(gc);
+        afterEach(gc);
 
-    var objcLib = new ffi.Library('libobjc', {
-      'objc_msgSend': [ id, [ id, SEL ] ],
-      'objc_getClass': [ Class, [ 'string' ] ],
-      'sel_registerName': [ SEL, [ 'string' ] ]
+        var objcLib = new ffi.Library('libobjc', {
+            'objc_msgSend': [id, [id, SEL]],
+            'objc_getClass': [Class, ['string']],
+            'sel_registerName': [SEL, ['string']]
+        });
+
+        // create an NSAutoreleasePool instance
+        var NSAutoreleasePool = objcLib.objc_getClass('NSAutoreleasePool');
+        var sel_new = objcLib.sel_registerName('new');
+        var pool = objcLib.objc_msgSend(NSAutoreleasePool, sel_new);
+
+        it('should proxy @try/@catch to JavaScript via try/catch/throw', function () {
+            var sel_retain = objcLib.sel_registerName('retain');
+            assert.throws(function () {
+                objcLib.objc_msgSend(pool, sel_retain);
+            });
+        });
+
+        it('should throw a Buffer instance when an exception happens', function () {
+            var sel_retain = objcLib.sel_registerName('retain');
+            assert.throws(function () {
+                objcLib.objc_msgSend(pool, sel_retain);
+            }, function (e) {
+                return Buffer.isBuffer(e) && !e.isNull() && e.address() > 0;
+            });
+        });
+
     });
-
-    // create an NSAutoreleasePool instance
-    var NSAutoreleasePool = objcLib.objc_getClass('NSAutoreleasePool');
-    var sel_new = objcLib.sel_registerName('new');
-    var pool = objcLib.objc_msgSend(NSAutoreleasePool, sel_new);
-
-    it('should proxy @try/@catch to JavaScript via try/catch/throw', function () {
-      var sel_retain = objcLib.sel_registerName('retain');
-      assert.throws(function () {
-        objcLib.objc_msgSend(pool, sel_retain);
-      });
-    });
-
-    it('should throw a Buffer instance when an exception happens', function () {
-      var sel_retain = objcLib.sel_registerName('retain');
-      assert.throws(function () {
-        objcLib.objc_msgSend(pool, sel_retain);
-      }, function (e) {
-        return Buffer.isBuffer(e)
-            && !e.isNull()
-            && e.address() > 0;
-      });
-    });
-
-  });
 
 }
